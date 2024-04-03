efa8b554b67f54951e4178b134d59c87a7350a85
Mustafa OKTA <57900964+mustafaokta@users.noreply.github.com>
2024-04-03 14:01:35 +0300

    Update DefaultHeader.tsx

--- pages/_layout/_headers/DefaultHeader.tsx
+++ pages/_layout/_headers/DefaultHeader.tsx
@@ -4,20 +4,40 @@ import Navigation from '../../../layout/Navigation/Navigation';
 import { componentPagesMenu, pageLayoutTypesPagesMenu } from '../../../menu';
 import useDeviceScreen from '../../../hooks/useDeviceScreen';
 import CommonHeaderRight from './CommonHeaderRight';
+import Avatar from '../../../components/Avatar';
+import UserImage2 from '../../../assets/img/wanna/wanna1.png';
+import { useUserContext } from '../../../context/UserContext';
+import { useDataUserRoleList } from '../../../helpers/connections/tour';
+
 
 const DefaultHeader = () => {
 	const deviceScreen = useDeviceScreen();
+	const {user}= useUserContext();
+   const { data: userRoleData, isLoading: userRoleIsLoading, isError: userRoleIsError } = useDataUserRoleList();
+      if (userRoleIsLoading ) return <div className="flex flex-col w-full">YÜKLENİYOR....</div>;
+      if (userRoleIsError ) return <div className="flex flex-col w-full">BİR HATA MEYDANA GELDİ....</div>;
 	return (
 		<Header>
 			<HeaderLeft>
-				<Navigation
+			<div className='col d-flex align-items-center'>
+					<div className='me-3'>
+						<Avatar src={UserImage2} size={48} color='primary' />
+					</div>
+					<div>
+						<div className='fw-bold fs-6 mb-0'>{user.name + ' ' + user.surname}</div>
+						<div className='text-muted'>
+							<small>{userRoleData.content.filter((el:any)=>el.id==user.roleId)[0].name  }</small>
+						</div>
+					</div>
+				</div>
+		{/* 		<Navigation
 					menu={{ ...pageLayoutTypesPagesMenu, ...componentPagesMenu }}
 					id='header-top-menu'
 					horizontal={
 						!!deviceScreen?.width &&
 						deviceScreen.width >= Number(process.env.NEXT_PUBLIC_MOBILE_BREAKPOINT_SIZE)
 					}
-				/>
+				/> */}
 			</HeaderLeft>
 			<CommonHeaderRight />
 		</Header>
