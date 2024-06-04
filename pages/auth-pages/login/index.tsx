135bd03d73854b6a03e14081c1a1dd2a3f04c999
Mustafa OKTA <57900964+mustafaokta@users.noreply.github.com>
2024-06-04 10:49:12 +0300

    logo brothers

--- pages/auth-pages/login/index.tsx
+++ pages/auth-pages/login/index.tsx
@@ -222,8 +222,9 @@ const Login: NextPage<ILoginProps> = ({ isSignUp }) => {
 												'text-light': darkModeStatus,
 											},
 										)}>
-										{process.env.NEXT_PUBLIC_COMPANY_NAME === 'Bigbrothers' && <Logo width={250} /> }
-										{process.env.NEXT_PUBLIC_COMPANY_NAME === 'Tigersson' && 'Tigersson Travel' }
+										<Logo width={250} /> 
+{/* 										{process.env.NEXT_PUBLIC_COMPANY_NAME === 'Bigbrothers' && <Logo width={250} /> }
+ */}										{/* {process.env.NEXT_PUBLIC_COMPANY_NAME === 'Tigersson' && 'Tigersson Travel' } */}
 									
 									</Link>
 								</div>
